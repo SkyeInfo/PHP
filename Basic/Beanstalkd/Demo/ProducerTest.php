61561eb82b663e0a2879b8e22718d406448449ff
SkyeInfo <skyeinfo@qq.com>
2018-01-15 21:02:59 +0800

    update

---
+++ Basic/Beanstalkd/Demo/ProducerTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 生产者模型
+ * @author skyeinfo@qq.com
+ * @LastModifyTime 2018-01-15
+ * @lastModifyBy skyeinfo@qq.com
+ */
+namespace Demo;
+class ProducerTest extends ConnectTest
+{
+    public function __construct() {}
+}
+?>
