9a555125dec641450372ddb17e6f812d5e4e236e
yangshengkai <yangshengkai@chuchujie.com>
2018-11-05 16:31:53 +0800

    add

--- Algo/PHP/linkedList/SingleLinkedList.php
+++ Algo/PHP/linkedList/SingleLinkedList.php
@@ -7,8 +7,6 @@
  */
 namespace Algo\PHP\linkedList;
 
-use Algo\PHP\linkedList\SingleLinkedListNode;
-
 class SingleLinkedList{
 
     /** @var 头结点（哨兵节点） */
@@ -30,9 +28,9 @@ class SingleLinkedList{
 
     /**
      * 获取链表长度
-     * @author yangshengkai@chuchujie.com
+     * @author skyeinfo@qq.com
      * @lastModifyTime 2018/10/23
-     * @lastModify yangshengkai@chuchujie.com
+     * @lastModify skyeinfo@qq.com
      * @return 单链表长度|int
      */
     public function getLength() {
@@ -41,9 +39,9 @@ class SingleLinkedList{
 
     /**
      * 在头结点后插入一条数据
-     * @author yangshengkai@chuchujie.com
+     * @author skyeinfo@qq.com
      * @lastModifyTime 2018/10/23
-     * @lastModify yangshengkai@chuchujie.com
+     * @lastModify skyeinfo@qq.com
      * @param $data
      * @return mixed
      */
@@ -52,10 +50,10 @@ class SingleLinkedList{
     }
 
     /**
-     * 在某个节点后插入新的节点（只接受值）
-     * @author yangshengkai@chuchujie.com
+     * 在某个节点后插入新的数据（只接受值）
+     * @author skyeinfo@qq.com
      * @lastModifyTime 2018/10/25
-     * @lastModify yangshengkai@chuchujie.com
+     * @lastModify skyeinfo@qq.com
      * @param \Algo\PHP\linkedList\SingleLinkedListNode $originNode
      * @param $data
      * @return \Algo\PHP\linkedList\SingleLinkedListNode|bool
@@ -80,9 +78,9 @@ class SingleLinkedList{
 
     /**
      * 删除某一个节点
-     * @author yangshengkai@chuchujie.com
+     * @author skyeinfo@qq.com
      * @lastModifyTime 2018/10/25
-     * @lastModify yangshengkai@chuchujie.com
+     * @lastModify skyeinfo@qq.com
      * @param \Algo\PHP\linkedList\SingleLinkedListNode $node
      * @return bool
      */
@@ -103,9 +101,9 @@ class SingleLinkedList{
 
     /**
      * 获取某节点的前一个节点
-     * @author yangshengkai@chuchujie.com
+     * @author skyeinfo@qq.com
      * @lastModifyTime 2018/10/25
-     * @lastModify yangshengkai@chuchujie.com
+     * @lastModify skyeinfo@qq.com
      * @param \Algo\PHP\linkedList\SingleLinkedListNode $node
      * @return 头结点（哨兵节点）|bool|null
      */
@@ -126,11 +124,116 @@ class SingleLinkedList{
         return $preNode;
     }
 
+    /**
+     * 通过索引获取节点
+     * @author skyeinfo@qq.com
+     * @lastModifyTime 2018/11/5
+     * @lastModify skyeinfo@qq.com
+     * @param $index
+     * @return null
+     */
     public function getNodeByIndex($index) {
         if ($index >= $this->length) {
             return null;
         }
+
+        $current = $this->head->next;
+        for ($i = 0; $i < $index; $i++) {
+            $current = $current->next;
+        }
+
+        return $current;
     }
 
+    /**
+     * 打印链表
+     * @author skyeinfo@qq.com
+     * @lastModifyTime 2018/11/5
+     * @lastModify skyeinfo@qq.com
+     * @return bool
+     */
+    public function printList() {
+        if (null == $this->head->next) {
+            return false;
+        }
 
+        $currNode = $this->head;
+
+        $listLength = $this->getLength();   //防止链表带环
+
+        while ($currNode->next != null && $listLength--) {
+            echo $currNode->next->data . ' -> ';
+
+            $currNode = $currNode->next;
+        }
+
+        echo 'NULL' . PHP_EOL;
+
+        return true;
+    }
+
+    /**
+     * 打印单链表
+     * @author skyeinfo@qq.com
+     * @lastModifyTime 2018/11/5
+     * @lastModify skyeinfo@qq.com
+     * @return bool
+     */
+    public function printListSimple() {
+        if (null == $this->head->next) {
+            return false;
+        }
+
+        $currNode = $this->head;
+        while ($currNode->next != null) {
+            echo $currNode->next->data . ' -> ';
+
+            $currNode = $currNode->next;
+        }
+        echo 'NULL' . PHP_EOL;
+
+        return true;
+    }
+
+    /**
+     * 在某节点前插入一个数据
+     * @author skyeinfo@qq.com
+     * @lastModifyTime 2018/11/5
+     * @lastModify skyeinfo@qq.com
+     * @param SingleLinkedListNode $originNode
+     * @param $data
+     * @return SingleLinkedListNode|bool
+     */
+    public function insertDataBefore(SingleLinkedListNode $originNode, $data) {
+        if (null == $originNode) {
+            return false;
+        }
+
+        $preNode = $this->getPreNode($originNode);
+
+        $rlt = $this->insertDataAfter($preNode, $data);
+
+        return $rlt;
+    }
+
+    /**
+     * 在某节点后添加一个新节点
+     * @author skyeinfo@qq.com
+     * @lastModifyTime 2018/11/5
+     * @lastModify skyeinfo@qq.com
+     * @param SingleLinkedListNode $originNode
+     * @param SingleLinkedListNode $node
+     * @return SingleLinkedListNode|bool
+     */
+    public function insertNodeAfter(SingleLinkedListNode $originNode, SingleLinkedListNode $node) {
+        if (null == $originNode) {
+            return false;
+        }
+        $node->next = $originNode->next;
+        $originNode->next = $node;
+
+        $this->length++;
+
+        return $node;
+    }
 }
